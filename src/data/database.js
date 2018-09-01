eef7e7fc3d6a004d2a33d5025052051457bcfc11
JhonasV <jhonas724@gmail.com>
2018-08-31 20:03:35 -0400

    mlab db added

--- src/data/database.js
+++ src/data/database.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/elecciones", ()=>{
+//mongodb://localhost:27017/elecciones
+mongoose.connect("mongodb://hola:hola1234@ds139632.mlab.com:39632/voteapp", ()=>{
     console.log("conectado a mongodb")
 }).catch(err => {
     console.error(err)
