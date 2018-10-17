77e9b2d1c507492a8dd11690569ff8239fdd20c3
Nelson Veras <nveras@TECHCOMMRD.LOCAL>
2018-10-17 11:28:09 -0400

    removing database info

--- src/data/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-//mongodb://localhost:27017/elecciones
-mongoose.connect("mongodb://hola:hola1234@ds139632.mlab.com:39632/voteapp", ()=>{
-    console.log("conectado a mongodb")
-}).catch(err => {
-    console.error(err)
-})
